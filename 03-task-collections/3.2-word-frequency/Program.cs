5b5dee422c190f935f43870b29e6d7e1325d6217
Vladimir Mihailov <vbuain@gmail.com>
2019-07-23 23:34:38 +0300

    no message

--- 03-task-collections/3.2-word-frequency/Program.cs
+++ 03-task-collections/3.2-word-frequency/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _3._2_word_frequency
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = 
+                "My cat eat meat and drink milk. And my dog eat meat and drink milk too. I like my cat and dog";
+            string low_text = text.ToLower(); //Перевод всех слов в нижний регистр
+
+
+            var list_text = low_text.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Split(new[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries).ToList())
+                .ToList();//Преобразование предложения в список
+
+
+            Console.ReadKey();//Delay
         }
     }
 }
