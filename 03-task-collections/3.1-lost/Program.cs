ae39dcd2ddeb3316341527aec3d998d158020327
Vladimir Mihailov <vbuain@gmail.com>
2019-07-23 23:49:37 +0300

    3.1 Done

--- 03-task-collections/3.1-lost/Program.cs
+++ 03-task-collections/3.1-lost/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _3._1_lost
 {
@@ -16,98 +17,13 @@ namespace _3._1_lost
         static void Main(string[] args)
         {
             var numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            
-            Display(numbers);
-            bool remove = false;
-            while(numbers.Count > 1)
+            Display(numbers.ToList());
+            while (numbers.Count > 1)
             {
-
-                for (int i = 0; i < numbers.Count; i++)
-                {
-                    
-                        if (i % 2 != 0)
-                        {
-                            if (remove)
-                            {
-
-                                numbers.RemoveAt(i);
-
-                            }
-                            else
-                            {
-                                remove = !remove;
-                            }
-                        }
-                        Display(numbers);
-                    
-
-
-                    //if (i % 2 != 0)
-                    //{
-                    //    if (remove)
-                    //    {
-                    //        //if (i % 2 != 0)
-                    //        //{
-                    //        numbers.RemoveAt(i);
-
-                    //    }
-
-                    //    //}
-                    //    //else
-                    //    //{
-                    //    //     remove = !remove;
-                    //    //}
-                    //}
-
-
-
-
-
-
-                    //if (remove)
-                    //{
-                    //    numbers.RemoveAt(i++);
-                    //}
-                    //else
-                    //{
-                    //    remove = !remove;
-                    //}
-                    //Display(numbers);
-                }
-                //Display(numbers);
+                numbers = numbers.Where((x, i) => i % 2 == 0).ToList();
+                Display(numbers);
             }
-
-            //foreach(int i in numbers)
-            //{
-            //    Console.Write(i + "  ");
-            //}
-            //Console.WriteLine();
-            //for (int i = 1; i <= numbers.Count; i++)
-            //{
-            //    if(i%2 == 0)
-            //    {
-            //        numbers.RemoveAt(i);
-            //    }
-            //    Console.Write(i + "  ");
-            //    i++;
-
-            //}
-            
             Console.ReadKey(); //Delay
-
-            //var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            //PrintList(list);
-            //bool delete = false;
-            //while (list.Count > 1)
-            //{
-            //    for (int i = 0; i < list.Count; i++)
-            //    {
-            //        if (delete) list.RemoveAt(i--);
-            //        delete = !delete;
-            //    }
-            //    PrintList(list);
-            //}
-            //Console.Read();
         }
     }
 }
