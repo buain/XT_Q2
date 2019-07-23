8c3613d1f20aa5d6b0ba0ed4f953336ab4712076
Vladimir Mihailov <vbuain@gmail.com>
2019-07-23 21:51:02 +0300

    In progress

--- 03-task-collections/3.1-lost/Program.cs
+++ 03-task-collections/3.1-lost/Program.cs
@@ -1,17 +1,113 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace _3._1_lost
 {
     class Program
     {
-
+        static void Display(IEnumerable<int> numbers)
+        {
+            foreach (var i in numbers)
+                Console.Write(i + "  ");
+            Console.WriteLine();
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите целое положительное нечетное число: ");
-            int input = Int32.Parse(Console.ReadLine());
+            var numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
+            Display(numbers);
+            bool remove = false;
+            while(numbers.Count > 1)
+            {
 
-            Console.WriteLine("Hello World!");
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    
+                        if (i % 2 != 0)
+                        {
+                            if (remove)
+                            {
+
+                                numbers.RemoveAt(i);
+
+                            }
+                            else
+                            {
+                                remove = !remove;
+                            }
+                        }
+                        Display(numbers);
+                    
+
+
+                    //if (i % 2 != 0)
+                    //{
+                    //    if (remove)
+                    //    {
+                    //        //if (i % 2 != 0)
+                    //        //{
+                    //        numbers.RemoveAt(i);
+
+                    //    }
+
+                    //    //}
+                    //    //else
+                    //    //{
+                    //    //     remove = !remove;
+                    //    //}
+                    //}
+
+
+
+
+
+
+                    //if (remove)
+                    //{
+                    //    numbers.RemoveAt(i++);
+                    //}
+                    //else
+                    //{
+                    //    remove = !remove;
+                    //}
+                    //Display(numbers);
+                }
+                //Display(numbers);
+            }
+
+            //foreach(int i in numbers)
+            //{
+            //    Console.Write(i + "  ");
+            //}
+            //Console.WriteLine();
+            //for (int i = 1; i <= numbers.Count; i++)
+            //{
+            //    if(i%2 == 0)
+            //    {
+            //        numbers.RemoveAt(i);
+            //    }
+            //    Console.Write(i + "  ");
+            //    i++;
+
+            //}
+            
+            Console.ReadKey(); //Delay
+
+            //var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //PrintList(list);
+            //bool delete = false;
+            //while (list.Count > 1)
+            //{
+            //    for (int i = 0; i < list.Count; i++)
+            //    {
+            //        if (delete) list.RemoveAt(i--);
+            //        delete = !delete;
+            //    }
+            //    PrintList(list);
+            //}
+            //Console.Read();
         }
     }
 }
--- 03-task-collections/3.3-dynamic-array/Program.cs
+++ 03-task-collections/3.3-dynamic-array/Program.cs
@@ -2,10 +2,30 @@
 
 namespace _3._3_dynamic_array
 {
+    public class DynamicArray<T>
+    {
+        private int count { get; set; }
+        private int user_count { get; set; }
+        public DynamicArray() //конструктор без параметров
+        {
+            count = 8; // Ёмкость массива
+        }
+        public DynamicArray(int User_count) //конструктор с одним целочисленным параметром
+        {
+            user_count = User_count; // 
+        }
+        public DynamicArray(IEnumerable<T> collection)
+        {
+            EnumArray = new T(collection.Count);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            DynamicArray<int> dynamicArray = new DynamicArray<int>(); // Создание динамического массива
+            int User_count = 16; // Указанная ёмкость массива
+            DynamicArray<int> user_dynamicArray = new DynamicArray<int>(User_count); // Создание массива указанной ёмкости
             Console.WriteLine("Hello World!");
         }
     }
