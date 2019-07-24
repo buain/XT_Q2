0c05d04e6b0d78675acdf82ac2736e0cd1f87bab
Vladimir Mihailov <vbuain@gmail.com>
2019-07-24 22:51:08 +0300

    In progress

--- 03-task-collections/3.3-dynamic-array/Program.cs
+++ 03-task-collections/3.3-dynamic-array/Program.cs
@@ -1,31 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _3._3_dynamic_array
 {
     public class DynamicArray<T>
     {
-        private int count { get; set; }
-        private int user_count { get; set; }
-        public DynamicArray() //конструктор без параметров
-        {
-            count = 8; // Ёмкость массива
-        }
-        public DynamicArray(int User_count) //конструктор с одним целочисленным параметром
-        {
-            user_count = User_count; // 
-        }
-        public DynamicArray(IEnumerable<T> collection)
-        {
-            EnumArray = new T(collection.Count);
-        }
+
+        //private int count { get; set; }
+        //private int user_count { get; set; }
+
+        //public DynamicArray() //конструктор без параметров
+        //{
+        //    count = 8; // Ёмкость массива
+        //}
+        //public DynamicArray(int User_count) //конструктор с одним целочисленным параметром
+        //{
+        //    user_count = User_count; // 
+        //}
+        ////public DynamicArray(IEnumerable<T> collection)
+        ////{
+        ////    EnumArray = new T(collection.Count);
+        ////}
+        //public void Add(DynamicArray<int>[] dynamicArray)
+        //{
+        //    dynamicArray.Add();
+        //}
     }
     class Program
     {
         static void Main(string[] args)
-        { 
-            DynamicArray<int> dynamicArray = new DynamicArray<int>(); // Создание динамического массива
-            int User_count = 16; // Указанная ёмкость массива
-            DynamicArray<int> user_dynamicArray = new DynamicArray<int>(User_count); // Создание массива указанной ёмкости
+        {
+            //DynamicArray<int> dynamicArray = new DynamicArray<int>(); // Создание массива
+            //var mass = new int[] { }; // массив
+
+
+            //int User_count = 16; // Указанная ёмкость массива
+            //DynamicArray<int> user_dynamicArray = new DynamicArray<int>(User_count); // Создание массива указанной ёмкости
+            //dynamicArray.Add(5);
             Console.WriteLine("Hello World!");
         }
     }
