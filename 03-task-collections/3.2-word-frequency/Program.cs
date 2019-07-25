51faa83218b9b867b82e016080b0f6e9c9bbb916
Vladimir Mihailov <vbuain@gmail.com>
2019-07-25 22:44:25 +0300

    no message

--- 03-task-collections/3.2-word-frequency/Program.cs
+++ 03-task-collections/3.2-word-frequency/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace _3._2_word_frequency
 {
     class Program
     {
-        public static void Clear(string items) //Clear string from punctuation and other simbols
+        public static void Clear(string items) // Clear string from punctuation and other simbols
         {
             string clear_words = null;
             for (int i = 0; i < items.Length; i++)
@@ -18,27 +17,48 @@ namespace _3._2_word_frequency
                 }
             }
             string[] array_words = clear_words.Split();
-            ToDict(array_words);
+            //AddList(array_words);
+            AddDict(array_words);
         }
-        public static void ToDict(string[] items)
-        {
+        //public static void AddList(string[] items) // Add array to list
+        //{
+        //    List<string> list_text = new List<string>();
 
+        //    foreach (var i in items)
+        //    {
+        //        list_text.Add(i);
+        //    }
+
+        //}
+        public static void AddDict(string[] items) // Add array to Dict
+        {
+           // Dictionary<string, int> dict = new Dictionary<string, int>();
+            int count = 0;
+            for(int i = 0; i < items.Length; i++)
+            {
+                if(items[i] == "and")
+                {
+                    count++;
+                }
+                Console.WriteLine($"Слово {items} повторялось {count} раз");
+            }
         }
+
 
         static void Main(string[] args)
         {
-            string text = 
-                "My cat eat meat and drink milk. And my dog eat meat and drink milk too. I like my cat and dog";
+            string text = "My cat eat meat and drink milk. And my dog eat meat and drink milk too. I like my cat and dog";
             Console.WriteLine($"Предложение: {text}");
             string low_text = text.ToLower(); //Перевод всех слов в нижний регистр
             Clear(low_text);
+            
 
             //var list_text = low_text.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries)
             //    .Select(x => x.Split(new[] { " ", "," }, StringSplitOptions.RemoveEmptyEntries).ToList())
             //    .ToList();//Преобразование предложения в список
 
 
-            Console.WriteLine(list_text);
+            //Console.WriteLine(list_text);
             
 
             Console.ReadKey();//Delay
--- 03-task-collections/3.3-dynamic-array/Program.cs
+++ 03-task-collections/3.3-dynamic-array/Program.cs
@@ -4,16 +4,34 @@ using System.Collections.Generic;
 
 namespace _3._3_dynamic_array
 {
-    public class List<T>
+    public interface ICollection<T> : IEnumerable<T>, IEnumerable
     {
-
+        //int Capacity { get; }
+        IEnumerator GetEnumerator();
     }
-    public class DynamicArray<T> : List<T>
+
+    public class DynamicArray<T>
     {
         private T[] mass;
-        
-        //private int user_count { get; set; }
-
+        private int length; //8-getting the number of items
+        public int Length
+        {
+            get
+            {
+                foreach (var i in mass)
+                {
+                    length++;
+                }
+                return length;
+            }
+            set
+            { length = value; }
+        }
+        private int capacity; //9-getting capacity: the length of the internal array
+        public int Capacity
+        {
+            set { capacity = 16; }
+        }
         public DynamicArray() // 1-constructor without parameters
         {
             mass = new T[8];
@@ -27,33 +45,71 @@ namespace _3._3_dynamic_array
             mass = new T[(collection as ICollection).Count];
             (collection as ICollection).CopyTo(mass, 0);
         }
-        //public static T[] Add<T>(T[] array, T item)
-        //{
-        //    T[] mass = new T[array.Length + 1];
-        //    for (int i = 0; i < array.Length; i++)
-        //    {
-        //        mass[i] = array[i];
-        //    }
-        //    mass[array.Length] = item;
-        //    return mass;
-        //}
-
-        public void Add(T[] mass, T newvalue) // 4-method that adds one element to the end of the array
+        public T[] Add(T[] array, T item) // 4-method that adds one element to the end of the array
         {
-            mass
+            T[] mass = new T[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                mass[i] = array[i];
+            }
+            mass[array.Length] = item;
+            return mass;
+        }
+        //public void AddRange(IEnumerable<T> collection, IEnumerable<T> new_collection) // 5-method of adding to the end of the array the contents of the collection that implements the interface
+        //{
+        //    DynamicArray<T> array = new_collection as DynamicArray<T>;
+        //    if(array != null)
+        //    {
+        //        array.AddRange(collection); // доделать
+        //    }
+        //    else
+        //    {
+        //        foreach (T item in collection)
+        //        {
+        //            array.Add(item); //доделать
+        //        }
+        //    }
+        //}
+        //private void Add(T Item) { } //доделать метод
+        //private void AddRange(IEnumerable<T> collection) { }
+        public bool Remove(IEnumerable<T> collection, T item) // 6-method that removes the specified item from the collection
+        {
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i].Equals(item))
+                {
+                    RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void RemoveAt(int index)
+        {
+            if(index >= capacity)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            int start = index + 1;
+            if (start < capacity)
+            {
+                Array.Copy(mass, start, mass, index, capacity - start);
+            }
+            capacity--;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
+            //int capacity = 
             //var mass = new int[] { }; // массив
-            //DynamicArray<int> dynamicArray = new DynamicArray<int>(); // Создание массива
+            DynamicArray<int> dynamicArray = new DynamicArray<int>(); // Создание массива
             
 
 
-            int User_count = 16; // Указанная ёмкость массива
-            DynamicArray<int> user_dynamicArray = new DynamicArray<int>(User_count); // Создание массива указанной ёмкости
+            //int User_count = 16; // Указанная ёмкость массива
+            //DynamicArray<int> user_dynamicArray = new DynamicArray<int>(User_count); // Создание массива указанной ёмкости
             //dynamicArray.Add(5);
             Console.WriteLine("Hello World!");
         }
