bed01b520e70f83527b6b0cb20e5671dc0e5fffb
Vladimir Mihailov <vbuain@gmail.com>
2019-07-29 16:30:23 +0300

    Task 4.6 done, 4.3 not yet

--- 04-task-delegates-and-extensions/4.3-sorting-unit/Program.cs
+++ 04-task-delegates-and-extensions/4.3-sorting-unit/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
 using System.Threading;
 
 namespace _4._3_sorting_unit
 {
-    delegate int[] delegate_sort<T>(int[] items);
     class Program
     {
-        public delegate void Sort_Handler(string message);
-        public event Sort_Handler EndSort; // 3-Event
+        delegate int[] delegate_sort<T>(int[] items);
+
+        //public delegate void Sort_Handler(string message);
+        //public event Sort_Handler EndSort; // 3-Event
         
-        private static int[] SortOverDelegate(int[] items) // 1-Sorting array
+        private static int[] Sort(int[] items) // 1-Sorting array
         {
             int temp = 0;
             for (int i = 0; i < items.Length - 1; i++)
@@ -25,6 +24,7 @@ namespace _4._3_sorting_unit
                         temp = items[i];
                         items[i] = items[j];
                         items[j] = temp;
+                        Thread.Sleep(400);
                     }
                 }
             }
@@ -32,7 +32,7 @@ namespace _4._3_sorting_unit
             //{
             //    EndSort("Sorting is done...");
             //}
-            EndSort?.Invoke("Sorting is done...");
+            //EndSort?.Invoke("Sorting is done...");
             return items;
         }
         //public event EventHandler<EventArgs> EndSort
@@ -40,6 +40,7 @@ namespace _4._3_sorting_unit
         //{
 
         //}
+
         static void Main(string[] args)
         {
             int[] array = { 4, 5, 6, 1, 9 };
@@ -49,14 +50,34 @@ namespace _4._3_sorting_unit
                 Console.Write(i + " ");
             }
 
-            delegate_sort<int> sort = SortOverDelegate;
+            delegate_sort<int> sort = Sort;
             int[] sorted_array = sort(array);
+            
 
             Console.WriteLine("\nSorted array:");
             foreach (int i in sorted_array)
             {
                 Console.Write(i + " ");
             }
+
+            //Anonymous method
+            delegate_sort<int> handler = delegate (int[] items)
+            {
+                for (int i = 0; i < items.Length; i++)
+                {
+                    items[i] = items[i] * 2;
+                    Thread.Sleep(500);
+                }
+                return items;
+            };
+            handler(array);
+
+            //ThreadStart _handler = new ThreadStart(handler);
+            Thread myThread = new Thread(handler);
+            myThread.Start();
+
+            
+
             Console.ReadKey(); //Delay;
         }
     }
--- 04-task-delegates-and-extensions/4.6-i-seek-you/Program.cs
+++ 04-task-delegates-and-extensions/4.6-i-seek-you/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Linq;
 
 namespace _4._6_i_seek_you
 {
     class Program
     {
+        delegate void SearchOverDelegate(int[] mass);
+        static void PositiveSearch(int[] items) // 1-Simple method of search
+        {
+            Console.WriteLine("Positive integers in array:");
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i] > 0)
+                {
+                    Console.Write(items[i] + ", ");
+                }
+            }
+            Console.WriteLine();
+        }
+        static void DisplayOriginalArray(int[] items)
+        {
+            Console.WriteLine("Original array:");
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.Write(items[i] + ", ");
+            }
+        }
         static void Main(string[] args)
         {
+            int[] arr = { 56, 74, -98, 0, 53, -5, 9, -15 };
+            PositiveSearch(arr);
+            // 2-Delegate instance:
+            SearchOverDelegate del = PositiveSearch;
+            del?.Invoke(arr);
+            // 3-Anonymous method:
+            SearchOverDelegate handler = delegate (int[] items)
+            {
+                Console.WriteLine("Anonymous method. Negative integers in array:");
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i] < 0)
+                    {
+                        Console.Write(items[i] + ", ");
+                    }
+                }
+                Console.WriteLine();
+            };
+            handler(arr);
+            // 4-Lambda expression:
+            SearchOverDelegate lambda = (massive) => DisplayOriginalArray(arr);
+            lambda(arr);
+            // 5-Using LINQ
+            int array_min = arr.Min();
+            Console.WriteLine($"\nMin in array: {array_min}");
+
             Console.ReadKey(); //Delay;
-        }
+         }
     }
 }
