ad076c5c5d67c92c5f869146de542c7156603778
Vladimir Mihailov <vbuain@gmail.com>
2019-08-12 23:27:25 +0300

    Let's start 6 task

---
+++ 06-task-design-patterns/6.1-users/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6._1_users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 06-task-design-patterns/6.2-and-awards/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6._2_and_awards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
