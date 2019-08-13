70d337b8409c8e4e07b05c9be1338e96ee82fce6
Vladimir Mihailov <vbuain@gmail.com>
2019-08-13 22:41:49 +0300

    In Process...

---
+++ 06-task-design-patterns/Users.BLL/UserList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Users.BLL
+{
+    public class UserList
+    {
+        public static GetAllUsers()
+        {
+
+        }
+        public static void AddUser()
+        {
+
+        }
+    }
+}
---
+++ 06-task-design-patterns/Users.PL/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Users.PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectToDo();
+            Console.ReadKey();
+        }
+        public static void SelectToDo()
+        {
+            Console.WriteLine("Choose what to do with user's info:");
+            Console.WriteLine("1.Create.");
+            Console.WriteLine("2.Delete.");
+            Console.WriteLine("3.View list of users");
+            Console.WriteLine("4.Exit.");
+
+            var input = Console.ReadLine();
+
+            if(uint.TryParse(input, out uint SelectedOption)
+                && SelectedOption > 0
+                && SelectedOption < 5)
+            {
+                switch (SelectedOption)
+                {
+                    case 1:
+                        //UserList.AddUser....
+                        SelectToDo();
+                        break;
+                    case 2:
+                        //UserList.DeleteUser....
+                        SelectToDo();
+                        break;
+                    case 3:
+                        //ShowUsers(UserList.GetAllUsers())
+                        SelectToDo();
+                        break;
+                    case 4:
+                        return;
+                }
+            }
+        }
+
+    }
+}
