81f4dec43352b4117e4e250512a85246e5dca096
Vladimir Mihailov <vbuain@gmail.com>
2019-09-04 23:55:09 +0300

    no message

--- 09-task-javascript-basics/9.4.Task/9.4.-script.js
+++ 09-task-javascript-basics/9.4.Task/9.4.-script.js
@@ -1,12 +1,47 @@
-function NextPage(){
-    
+var url, ViewTime, TimeOutID, _progress, _button, pause=false;
+
+function NextPage(address){
+    location.assign(address);
 }
-function Pause(){
-    
+
+function Pause(addr){
+    if(!pause){
+		pause = true;
+		_button = document.getElementById("pause");
+		_button.innerHTML = "Продолжить";
+		clearTimeout(TimeOutID);
+	}
+	else{
+		pause = false;
+		_button = document.getElementById("pause");
+		_button.innerHTML = "Стоп";
+		Progress(addr, ViewTime);
+	}
 }
-function Progress(){
-    
+function Progress(addr, _time){
+	var a = "" + addr,b,bar;
+	
+    if(a.match(/.+\.html/)){
+		url = "" + a;
+	}
+	b = +_time;
+	
+	if(ViewTime === undefined){
+		ViewTime = +b;
+	}
+	bar = document.getElementById("LeftTime");
+	bar.innerHTML = ViewTime;
+	ViewTime--;
+	
+	if(ViewTime > 0){
+		TimeOutID = setTimeout(Progress, 1000);
+	}
+	else{
+		NextPage(url);
+	}
 }
-function Stop{
-    
+
+function Stop(){
+	clearTimeout(TimeOutID);
+    ViewTime = undefined;
 }
