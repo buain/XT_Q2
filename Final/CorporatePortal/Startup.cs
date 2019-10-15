06863208d68a41165d5c2fa1c3b614429da08848
Vladimir Mihailov <vbuain@gmail.com>
2019-10-16 00:11:21 +0300

    its starts a new pure project

---
+++ Final/CorporatePortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CorporatePortal.Startup))]
+namespace CorporatePortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
